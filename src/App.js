c6e8c016fa61f828ffae2e4fabc8b695a50d2ab0
guridaek <xylitol79@gmail.com>
2023-12-06 16:35:21 +0900

    feat: App 컴포넌트 추가

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App({ $target }) {
+  this.state = {
+    toDoList: [],
+    inProgressList: [],
+    doneList: [],
+  };
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "../style/reset.css";
+import "../style/global.css";
+
+import App from "./App.js";
+
+new App({ $parent: document.querySelector("App") });

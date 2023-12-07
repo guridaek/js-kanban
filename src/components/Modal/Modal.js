5415207fd361428709a36dbf963149c29e9f6fc0
guridaek <xylitol79@gmail.com>
2023-12-07 13:53:44 +0900

    feat: Modal 기능 구현

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,23 +1,38 @@
 import "./Modal.css";
 
-function Modal({ $target }) {
+function Modal({ $target, handleClickConfirm }) {
   this.$element = document.createElement("dialog");
-  this.$element.className = "modal";
   $target.appendChild(this.$element);
 
   this.render = () => {
     this.$element.innerHTML = `
-      <h2>항목 추가/수정</h2>
-      <label>이슈 제목<input id="issue-title" placeholder="이슈 제목을 입력해주세요" /></label>
-      <label>담당자 id<input id="manager-id" placeholder="담당자 id를 입력해주세요" /></label>
-      <div class="buttonContainer">
-      <button>취소</button>
-      <button>확인</button>
-      </div>
+      <form class="modal">
+        <h2>항목 추가/수정</h2>
+        <label>이슈 제목<input id="issue-title" placeholder="이슈 제목을 입력해주세요" /></label>
+        <label>담당자 id<input id="manager-id" placeholder="담당자 id를 입력해주세요" /></label>
+        <div class="buttonContainer">
+          <button id="cancel" type="reset">취소</button>
+          <button id="confirm" type="reset">확인</button>
+        </div>
+      </form>
     `;
   };
 
   this.render();
+
+  this.$element.addEventListener("click", (e) => {
+    if (e.target === e.currentTarget) this.$element.close();
+  });
+
+  this.$element.querySelector("#cancel").addEventListener("click", () => {
+    this.$element.close();
+  });
+
+  this.$element.querySelector("#confirm").addEventListener("click", () => {
+    handleClickConfirm();
+
+    this.$element.close();
+  });
 }
 
 export default Modal;

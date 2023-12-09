eb4f2f05a60172d82032f4dcdf23b92fe38d00f8
guridaek <xylitol79@gmail.com>
2023-12-09 15:36:08 +0900

    refactor: Modal 리팩터링

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -18,74 +18,29 @@ function Modal({ $target, addIssue, modifyIssue }) {
     };
   };
 
-  this.render = () => {
-    this.$element.innerHTML = `
-      <form class="modal">
-        <h2>항목 추가/수정</h2>
-        <label
-          >이슈 제목<input id="issueTitle" placeholder="이슈 제목을 입력해주세요" />
-          <p id="issueTitleError" class="errorMessage hide">이슈 제목은 필수 값입니다.</p></label
-        >
-
-        <label
-          >담당자 id<input id="managerId" placeholder="담당자 id를 입력해주세요" />
-          <p id="managerIdError" class="errorMessage hide">담당자 id는 필수 값입니다.</p></label
-        >
-
-        <div class="buttonContainer">
-          <button id="cancel" type="reset">취소</button>
-          <button id="confirm" type="reset">확인</button>
-        </div>
-      </form>
-    `;
+  const showError = (element, shouldShow) => {
+    element.classList.toggle("error", shouldShow);
   };
 
-  this.render();
-
-  this.open = ({ action, issueNumber, title, managerId, status }) => {
-    this.setState({ action, issueNumber, status });
-
-    if (title) this.$element.querySelector("#issueTitle").value = title;
-    if (managerId) this.$element.querySelector("#managerId").value = managerId;
-
-    this.$element.showModal();
-  };
-
-  this.close = () => {
-    this.$element.close();
-
-    this.$element.querySelector("#issueTitle").classList.remove("error");
-    this.$element.querySelector("#managerId").classList.remove("error");
-    this.$element.querySelector(".modal").reset();
-  };
-
-  this.$element.addEventListener("click", (e) => {
-    if (e.target === e.currentTarget) this.close();
-  });
-
-  this.$element.querySelector("#cancel").addEventListener("click", () => {
+  const handleCancelButtonClick = () => {
     this.close();
-  });
+  };
 
-  this.$element.querySelector("#confirm").addEventListener("click", (e) => {
+  const handleConfirmButtonClick = () => {
+    const issueTitle = this.$element.querySelector("#issueTitle");
+    const managerId = this.$element.querySelector("#managerId");
+
+    showError(issueTitle, !issueTitle.value.trim());
+    showError(managerId, !managerId.value.trim());
+
+    if (!issueTitle.value.trim() || !managerId.value.trim()) {
+      return;
+    }
+
     const newIssue = {
-      title: this.$element.querySelector("#issueTitle").value,
-      managerId: this.$element.querySelector("#managerId").value,
+      title: issueTitle.value,
+      managerId: managerId.value,
     };
-
-    if (newIssue.title.trim() === "") {
-      this.$element.querySelector("#issueTitle").classList.add("error");
-
-      e.preventDefault();
-      return;
-    }
-
-    if (newIssue.managerId.trim() === "") {
-      this.$element.querySelector("#managerId").classList.add("error");
-
-      e.preventDefault();
-      return;
-    }
 
     if (this.state.action === "add") {
       addIssue(newIssue);
@@ -96,6 +51,67 @@ function Modal({ $target, addIssue, modifyIssue }) {
     }
 
     this.close();
+  };
+
+  this.render = () => {
+    this.$element.innerHTML = `
+      <form class="modal">
+        <h2>항목 추가/수정</h2>
+        <label>
+          이슈 제목<input id="issueTitle" placeholder="이슈 제목을 입력해주세요" />
+          <p id="issueTitleError" class="errorMessage hide">이슈 제목은 필수 값입니다.</p>
+        </label>
+
+        <label>
+          담당자 id<input id="managerId" placeholder="담당자 id를 입력해주세요" />
+          <p id="managerIdError" class="errorMessage hide">담당자 id는 필수 값입니다.</p>
+        </label>
+
+        <div class="buttonContainer">
+          <button id="cancel" type="button">취소</button>
+          <button id="confirm" type="button">확인</button>
+        </div>
+      </form>
+    `;
+  };
+
+  this.render();
+
+  this.open = ({ action, issueNumber, title, managerId, status }) => {
+    this.setState({ action, issueNumber, status });
+
+    const issueTitle = this.$element.querySelector("#issueTitle");
+    const managerIdInput = this.$element.querySelector("#managerId");
+
+    issueTitle.value = title || "";
+    managerIdInput.value = managerId || "";
+
+    this.$element.showModal();
+  };
+
+  this.close = () => {
+    this.$element.querySelector(".modal").reset();
+    this.$element.close();
+
+    ["#issueTitle", "#managerId"].forEach((selector) => {
+      showError(this.$element.querySelector(selector), false);
+    });
+  };
+
+  this.$element.addEventListener("click", (e) => {
+    if (e.target === e.currentTarget) {
+      this.close();
+    }
+  });
+
+  this.$element.addEventListener("click", (e) => {
+    if (e.target.id === "cancel") {
+      handleCancelButtonClick();
+    }
+
+    if (e.target.id === "confirm") {
+      handleConfirmButtonClick();
+    }
   });
 }
 

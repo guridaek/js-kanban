14ffdb99274207a4b9d04829b77d4001393942d3
guridaek <xylitol79@gmail.com>
2023-12-10 13:52:23 +0900

    feat: 드래그 앤 드롭 테스트 추가

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,19 @@
 import mockData from "../fixtures/issueList.json";
 import { LOCAL_STORAGE_KEYS } from "../../src/data/handler";
+import { STATUS } from "../../src/data/constants";
+
+const setupTestData = () => {
+  const size = mockData.toDoList.length + mockData.inProgressList.length + mockData.doneList.length;
+
+  localStorage.setItem(LOCAL_STORAGE_KEYS.NEXT_NUMBER, size.toString());
+  localStorage.setItem(LOCAL_STORAGE_KEYS.ISSUE_LIST, JSON.stringify(mockData));
+};
 
 describe("칸반 보드를 테스트한다", () => {
   beforeEach(() => {
-    const size =
-      mockData.toDoList.length + mockData.inProgressList.length + mockData.doneList.length;
+    setupTestData();
 
-    localStorage.setItem(LOCAL_STORAGE_KEYS.NEXT_NUMBER, size.toString());
-    localStorage.setItem(LOCAL_STORAGE_KEYS.ISSUE_LIST, JSON.stringify(mockData));
-
+    cy.viewport(900, 900);
     cy.visit("/");
   });
 
@@ -18,9 +23,9 @@ describe("칸반 보드를 테스트한다", () => {
   });
 
   it("칸반 보드를 렌더링한다", () => {
-    cy.contains(mockData.toDoList[0].issueNumber).should("be.visible");
-    cy.contains(mockData.inProgressList[1].issueNumber).should("be.visible");
-    cy.contains(mockData.doneList[2].issueNumber).should("be.visible");
+    cy.get(`[data-issue-number="${mockData.toDoList[0].issueNumber}"]`).should("be.visible");
+    cy.get(`[data-issue-number="${mockData.inProgressList[1].issueNumber}"]`).should("be.visible");
+    cy.get(`[data-issue-number="${mockData.doneList[2].issueNumber}"]`).should("be.visible");
   });
 
   it("이슈를 추가한다", () => {
@@ -30,7 +35,9 @@ describe("칸반 보드를 테스트한다", () => {
     cy.get('input[id="managerId"]').type("담당자 이름");
     cy.get('button[id="confirm"]').click();
 
-    cy.contains("추가된 이슈").should("be.visible");
+    cy.get(`ul[data-status="${STATUS.TODO}"] > :nth-child(4)`)
+      .contains("추가된 이슈")
+      .should("be.visible");
   });
 
   it("이슈를 제거한다", () => {
@@ -47,7 +54,7 @@ describe("칸반 보드를 테스트한다", () => {
     cy.get('input[id="managerId"]').type("담당자 이름");
     cy.get('button[id="confirm"]').click();
 
-    cy.contains("수정된 이슈").should("be.visible");
+    cy.get('[data-issue-number="ISSUE-003"]').contains("수정된 이슈").should("be.visible");
   });
 
   it("이슈 추가 및 수정은 제목과 담당자 ID를 입력해야한다", () => {
@@ -56,6 +63,30 @@ describe("칸반 보드를 테스트한다", () => {
     cy.get('input[id="issueTitle"]').type("추가된 이슈");
     cy.get('button[id="confirm"]').click();
 
-    cy.contains("필수 값입니다").should("be.visible");
+    cy.get(".modal").should("be.visible");
+  });
+
+  it("드래그 앤 드롭으로 이슈의 순서를 변경한다", () => {
+    cy.moveIssue("ISSUE-002", "ISSUE-000");
+
+    cy.get(`ul[data-status="${STATUS.TODO}"] > :nth-child(1)`)
+      .contains("ISSUE-002")
+      .should("be.visible");
+
+    cy.get(`ul[data-status="${STATUS.TODO}"] > :nth-child(3)`)
+      .contains("ISSUE-001")
+      .should("be.visible");
+  });
+
+  it("드래그 앤 드롭으로 이슈의 그룹을 변경한다", () => {
+    cy.moveIssue("ISSUE-000", "ISSUE-006");
+
+    cy.get(`ul[data-status="${STATUS.DONE}"] > :nth-child(1)`)
+      .contains("ISSUE-000")
+      .should("be.visible");
+
+    cy.get(`ul[data-status="${STATUS.TODO}"] > :nth-child(1)`)
+      .contains("ISSUE-001")
+      .should("be.visible");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,29 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("moveIssue", (sourceIssueNumber, targetIssueNumber) => {
+  const dataTransfer = new DataTransfer();
+
+  let kanbanX, kanbanY;
+  let issueX, issueY;
+
+  cy.get(".kanban")
+    .then(($element) => {
+      const { x, y } = $element[0].getBoundingClientRect();
+
+      kanbanX = x;
+      kanbanY = y;
+
+      return cy.get(`[data-issue-number="${sourceIssueNumber}"]`);
+    })
+    .then(($element) => {
+      const { x, y } = $element[0].getBoundingClientRect();
+
+      issueX = x;
+      issueY = y;
+    })
+    .then(() => {
+      cy.get(".kanban").trigger("dragstart", issueX - kanbanX, issueY - kanbanY, {
+        dataTransfer,
+      });
+
+      cy.get(`[data-issue-number="${targetIssueNumber}"]`).trigger("drop", { dataTransfer });
+    });
+});
